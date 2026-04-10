c7dce90ef3779d30a8c9f3b35c31941c84187e00
vrt88 <140164658+vrt88@users.noreply.github.com>
2026-04-10 12:05:40 +0200

    Aggiunta file base

---
+++ parser.py
@@ -0,0 +1,46 @@
+from crawl4ai import AsyncWebCrawler, BrowserConfig, CrawlerRunConfig, CacheMode, DefaultMarkdownGenerator
+from abc import ABC,abstractmethod
+class Parser(ABC):
+    def __init__(self):
+        pass
+    async def parse(self, url : str) -> dict:
+        pass
+class ParserWikipediaIT(Parser):
+    def __init__(self):
+        self.domain = "it.wikipedia.org"
+    async def parse(self, url : str) -> dict:
+        html_text = ""
+        browser_cfg = BrowserConfig(headless=False)
+        crawler_cfg_html = CrawlerRunConfig(cache_mode=CacheMode.BYPASS)
+        clean_sup_script = """document.querySelectorAll('sup').forEach(el => el.remove());"""
+        crawler_cfg_parsed = CrawlerRunConfig(
+            js_code=clean_sup_script,
+            cache_mode=CacheMode.BYPASS,
+            markdown_generator=DefaultMarkdownGenerator(options={"ignore_links": True}),
+            css_selector=".firstHeading,.mw-body-content",
+            excluded_selector=".hatnote,[aria-labelledby='Note'],[aria-labelledby='Note'] ~ *,.mw-editsection,.infobox",
+            excluded_tags=["form", "header", "footer", "nav", "script", "style", "figure", "sup", "img"],
+            exclude_external_links=True,    
+            exclude_social_media_links=True,
+            exclude_external_images=True
+            )
+
+        async with AsyncWebCrawler(config=browser_cfg) as crawler:
+            result = await crawler.arun(
+                url = url,
+                config = crawler_cfg_html
+            )
+            html_text = result.html
+            result = await crawler.arun(
+                url = f"raw:{html_text}",
+                config = crawler_cfg_parsed
+            )
+        if not result.success:
+            raise RuntimeError(result.error_message)  
+        return{
+            "url":url,
+            "domain":self.domain,
+            "title":result.markdown.split("\n",1)[0].replace("# ",""),
+            "html_text":html_text,
+            "parsed_text":result.markdown.split("\n",1)[1]
+        }
---
+++ server.py
@@ -0,0 +1,8 @@
+import asyncio
+from parser import ParserWikipediaIT
+async def main():
+    parser = ParserWikipediaIT()
+    ret = await parser.parse("https://it.wikipedia.org/wiki/Minerva")
+    print(ret["parsed_text"].replace("## ", "\n").replace("_", "").replace("**", ""))
+if __name__ == "__main__":
+    asyncio.run(main())
