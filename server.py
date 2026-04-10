e8f551ae73a6bb6353d2eb03371091c00b6eecd5
Ray1745 <267098866+Ray1745@users.noreply.github.com>
2026-04-10 13:22:31 +0200

    ho cambiato link in riga 5 del file server.py

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import asyncio
 from parser import ParserWikipediaIT
 async def main():
     parser = ParserWikipediaIT()
-    ret = await parser.parse("https://it.wikipedia.org/wiki/Minerva")
+    ret = await parser.parse("https://it.wikipedia.org/wiki/Jo%C3%A3o_Fonseca_(tennista)")
     print(ret["parsed_text"].replace("## ", "\n").replace("_", "").replace("**", ""))
 if __name__ == "__main__":
     asyncio.run(main())
