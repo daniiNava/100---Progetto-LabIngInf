65b1aa690d9428b1a41a1bea160a0957c3172e84
vrt88 <140164658+vrt88@users.noreply.github.com>
2026-04-15 13:53:46 +0200

    Aggiornamento parser e GS Repubblica

--- parser.py
+++ parser.py
@@ -4,21 +4,16 @@ from abc import ABC,abstractmethod
 class Parser(ABC):
     def __init__(self):
         pass
-    async def parse(self, url : str) -> dict:
+    async def parse_by_url(self, url : str) -> dict:
+        pass
+    async def parse_by_gs(self, gs_json : dict) -> dict:
         pass
 
 class ParserWikipediaIT(Parser):
     def __init__(self):
         self.domain = "it.wikipedia.org"
-
-    async def parse(self, url : str) -> dict:
-        html_text = ""
-        title = ""
-        clean_sup_script = """document.querySelectorAll('sup').forEach(el => el.remove());"""
-
-        browser_cfg = BrowserConfig(headless=False)
-        crawler_cfg_parsed = CrawlerRunConfig(
-            js_code=clean_sup_script,
+        self.crawler_cfg_parsed = CrawlerRunConfig(
+            js_code="""document.querySelectorAll('sup').forEach(el => el.remove());""",
             cache_mode=CacheMode.BYPASS,
             markdown_generator=DefaultMarkdownGenerator(options={"ignore_links": True}),
             css_selector=".mw-body-content",
@@ -29,15 +24,23 @@ class ParserWikipediaIT(Parser):
             exclude_external_images=True
             )
 
+    async def parse_by_url(self, url : str) -> dict:
+        html_text = ""
+        title = ""
+
+        browser_cfg = BrowserConfig(headless=False)
+        
         async with AsyncWebCrawler(config=browser_cfg) as crawler:
             result = await crawler.arun(
                 url = url
             )
+            if not result.success:
+                raise RuntimeError(result.error_message)
             html_text = result.html
             title = result.metadata.get('og:title')
             result = await crawler.arun(
                 url = f"raw:{html_text}",
-                config = crawler_cfg_parsed
+                config = self.crawler_cfg_parsed
             )
         if not result.success:
             raise RuntimeError(result.error_message)
@@ -48,15 +51,30 @@ class ParserWikipediaIT(Parser):
             "html_text":html_text,
             "parsed_text":result.markdown.rsplit("\n",2)[0]
         }
+    async def parse_by_gs(self, gs_json : dict) -> dict:
+        html_text = gs_json["html_text"]
+
+        browser_cfg = BrowserConfig(headless=False)
+        
+        async with AsyncWebCrawler(config=browser_cfg) as crawler:
+            result = await crawler.arun(
+                url = f"raw:{html_text}",
+                config = self.crawler_cfg_parsed
+            )
+        if not result.success:
+            raise RuntimeError(result.error_message)
+        return{
+            "url":gs_json["url"],
+            "domain":self.domain,
+            "title":gs_json["title"],
+            "html_text":html_text,
+            "parsed_text":result.markdown.rsplit("\n",2)[0]
+        }
 
 class ParserBBC(Parser):
     def __init__(self):
         self.domain = "https://www.bbc.com"
-    async def parse(self, url : str) -> dict:
-        html_text = ""
-        title = ""
-        browser_cfg = BrowserConfig(headless=False)
-        crawler_cfg_parsed = CrawlerRunConfig(
+        self.crawler_cfg_parsed = CrawlerRunConfig(
             cache_mode=CacheMode.BYPASS,
             markdown_generator=DefaultMarkdownGenerator(options={"ignore_links": True}),
             css_selector="#main-content,.HooNV,.fWTqWp",
@@ -65,16 +83,22 @@ class ParserBBC(Parser):
             exclude_social_media_links=True,
             exclude_external_images=True
             )
+    async def parse_by_url(self, url : str) -> dict:
+        html_text = ""
+        title = ""
+        browser_cfg = BrowserConfig(headless=False)
 
         async with AsyncWebCrawler(config=browser_cfg) as crawler:
             result = await crawler.arun(
                 url = url
             )
+            if not result.success:
+                raise RuntimeError(result.error_message)
             html_text = result.html
             title = result.metadata.get('og:title')
             result = await crawler.arun(
                 url = f"raw:{html_text}",
-                config = crawler_cfg_parsed
+                config = self.crawler_cfg_parsed
             )
         if not result.success:
             raise RuntimeError(result.error_message)
@@ -85,15 +109,30 @@ class ParserBBC(Parser):
             "html_text":html_text,
             "parsed_text":result.markdown.rsplit("--",2)[0]
         }
+    async def parse_by_gs(self, gs_json : dict) -> dict:
+        html_text = gs_json["html_text"]
+
+        browser_cfg = BrowserConfig(headless=False)
+
+        async with AsyncWebCrawler(config=browser_cfg) as crawler:
+            result = await crawler.arun(
+                url = f"raw:{html_text}",
+                config = self.crawler_cfg_parsed
+            )
+        if not result.success:
+            raise RuntimeError(result.error_message)
+        return{
+            "url":gs_json["url"],
+            "domain":self.domain,
+            "title":gs_json["title"],
+            "html_text":html_text,
+            "parsed_text":result.markdown.rsplit("--",2)[0]
+        }
 
 class ParserPeople(Parser):
     def __init__(self):
         self.domain = "https://people.com"
-    async def parse(self, url : str) -> dict:
-        html_text = ""
-        title = ""
-        browser_cfg = BrowserConfig(headless=False)
-        crawler_cfg_parsed = CrawlerRunConfig(
+        self.crawler_cfg_parsed = CrawlerRunConfig(
             cache_mode=CacheMode.BYPASS,
             markdown_generator=DefaultMarkdownGenerator(options={"ignore_links": True}),
             css_selector=".article-subheading,#article-content_1-0",
@@ -103,6 +142,10 @@ class ParserPeople(Parser):
             exclude_social_media_links=True,
             exclude_external_images=True
             )
+    async def parse_by_url(self, url : str) -> dict:
+        html_text = ""
+        title = ""
+        browser_cfg = BrowserConfig(headless=False)
 
         async with AsyncWebCrawler(config=browser_cfg) as crawler:
             result = await crawler.arun(
@@ -112,7 +155,7 @@ class ParserPeople(Parser):
             title = result.metadata.get('og:title')
             result = await crawler.arun(
                 url = f"raw:{html_text}",
-                config = crawler_cfg_parsed
+                config = self.crawler_cfg_parsed
             )
         if not result.success:
             raise RuntimeError(result.error_message)
@@ -123,15 +166,30 @@ class ParserPeople(Parser):
             "html_text":html_text,
             "parsed_text":result.markdown
         }
+    async def parse_by_gs(self, gs_json : dict) -> dict:
+        html_text = gs_json["html_text"]
+
+        browser_cfg = BrowserConfig(headless=False)
+
+        async with AsyncWebCrawler(config=browser_cfg) as crawler:
+            result = await crawler.arun(
+                url = f"raw:{html_text}",
+                config = self.crawler_cfg_parsed
+            )
+        if not result.success:
+            raise RuntimeError(result.error_message)
+        return{
+            "url":gs_json["url"],
+            "domain":self.domain,
+            "title":gs_json["title"],
+            "html_text":html_text,
+            "parsed_text":result.markdown
+        }
 
 class ParserRepubblica(Parser):
     def __init__(self):
         self.domain = "https://www.repubblica.it"
-    async def parse(self, url : str) -> dict:
-        html_text = ""
-        title = ""
-        browser_cfg = BrowserConfig(headless=False)
-        crawler_cfg_parsed = CrawlerRunConfig(
+        self.crawler_cfg_parsed = CrawlerRunConfig(
             cache_mode=CacheMode.BYPASS,
             markdown_generator=DefaultMarkdownGenerator(options={"ignore_links": True}),
             css_selector=".story__text,.story__summary",
@@ -141,7 +199,11 @@ class ParserRepubblica(Parser):
             exclude_social_media_links=True,
             exclude_external_images=True
             )
-
+    async def parse_by_url(self, url : str) -> dict:
+        html_text = ""
+        title = ""
+        browser_cfg = BrowserConfig(headless=False)
+        
         async with AsyncWebCrawler(config=browser_cfg) as crawler:
             result = await crawler.arun(
                 url = url
@@ -150,7 +212,7 @@ class ParserRepubblica(Parser):
             title = result.metadata.get('og:title')
             result = await crawler.arun(
                 url = f"raw:{html_text}",
-                config = crawler_cfg_parsed
+                config = self.crawler_cfg_parsed
             )
         if not result.success:
             raise RuntimeError(result.error_message)
@@ -158,6 +220,25 @@ class ParserRepubblica(Parser):
             "url":url,
             "domain":self.domain,
             "title":title,
+            "html_text":html_text,
+            "parsed_text":result.markdown
+        }
+    async def parse_by_gs(self, gs_json : dict) -> dict:
+        html_text = gs_json["html_text"]
+
+        browser_cfg = BrowserConfig(headless=False)
+
+        async with AsyncWebCrawler(config=browser_cfg) as crawler:
+            result = await crawler.arun(
+                url = f"raw:{html_text}",
+                config = self.crawler_cfg_parsed
+            )
+        if not result.success:
+            raise RuntimeError(result.error_message)
+        return{
+            "url":gs_json["url"],
+            "domain":self.domain,
+            "title":gs_json["title"],
             "html_text":html_text,
             "parsed_text":result.markdown
         }
--- server.py
+++ server.py
@@ -5,7 +5,7 @@ from tokenLevelEval import TokenLevelEval
 import json
 async def testWikipediaIT():
     parser = ParserWikipediaIT()
-    ret = await parser.parse("https://it.wikipedia.org/wiki/Minerva")
+    ret = await parser.parse_by_url("https://it.wikipedia.org/wiki/Minerva")
     parsed_text_cleaned = MarkdownCleaner.clean(ret["parsed_text"])
 
     with open('GS/WikipediaIT.json', 'r', encoding='utf-8') as gs:
@@ -14,7 +14,7 @@ async def testWikipediaIT():
     result = TokenLevelEval.eval(parsed_text_cleaned,gold_text)
     print(f"{ret["title"]} : {result:2f}")
 
-    ret = await parser.parse("https://it.wikipedia.org/wiki/BabelNet")
+    ret = await parser.parse_by_url("https://it.wikipedia.org/wiki/BabelNet")
     parsed_text_cleaned = MarkdownCleaner.clean(ret["parsed_text"])
 
     with open('GS/WikipediaIT.json', 'r', encoding='utf-8') as gs:
@@ -25,7 +25,7 @@ async def testWikipediaIT():
 
 async def testBBC():
     parser = ParserBBC()
-    ret = await parser.parse("https://www.bbc.com/news/world-us-canada-34317754")
+    ret = await parser.parse_by_url("https://www.bbc.com/news/world-us-canada-34317754")
     parsed_text_cleaned = MarkdownCleaner.clean(ret["parsed_text"])
     
     with open('GS/BBC.json', 'r', encoding='utf-8') as gs:
@@ -34,7 +34,7 @@ async def testBBC():
     result = TokenLevelEval.eval(parsed_text_cleaned,gold_text)
     print(f"{ret["title"]} : {result:2f}")
 
-    ret = await parser.parse("https://www.bbc.com/news/articles/c20q4nv89yzo")
+    ret = await parser.parse_by_url("https://www.bbc.com/news/articles/c20q4nv89yzo")
     parsed_text_cleaned = MarkdownCleaner.clean(ret["parsed_text"])
     
     with open('GS/BBC.json', 'r', encoding='utf-8') as gs:
@@ -43,7 +43,7 @@ async def testBBC():
     result = TokenLevelEval.eval(parsed_text_cleaned,gold_text)
     print(f"{ret["title"]} : {result:2f}")
 
-    ret = await parser.parse("https://www.bbc.com/travel/article/20260410-why-its-impossible-to-measure-englands-coastline")
+    ret = await parser.parse_by_url("https://www.bbc.com/travel/article/20260410-why-its-impossible-to-measure-englands-coastline")
     parsed_text_cleaned = MarkdownCleaner.clean(ret["parsed_text"])
     
     with open('GS/BBC.json', 'r', encoding='utf-8') as gs:
@@ -52,7 +52,7 @@ async def testBBC():
     result = TokenLevelEval.eval(parsed_text_cleaned,gold_text)
     print(f"{ret["title"]} : {result:2f}")
 
-    ret = await parser.parse("https://www.bbc.com/culture/article/20260216-10-early-photographic-fakes-that-trick-the-eye")
+    ret = await parser.parse_by_url("https://www.bbc.com/culture/article/20260216-10-early-photographic-fakes-that-trick-the-eye")
     parsed_text_cleaned = MarkdownCleaner.clean(ret["parsed_text"])
     
     with open('GS/BBC.json', 'r', encoding='utf-8') as gs:
@@ -63,7 +63,7 @@ async def testBBC():
 
 async def testRepubblica():
     parser = ParserRepubblica()
-    ret = await parser.parse("https://www.repubblica.it/tecnologia/2023/12/12/news/startup_italiane_10_al_top-421587498/")
+    ret = await parser.parse_by_url("https://www.repubblica.it/tecnologia/2023/12/12/news/startup_italiane_10_al_top-421587498/")
     parsed_text_cleaned = MarkdownCleaner.clean(ret["parsed_text"])
     
     with open('GS/Repubblica.json', 'r', encoding='utf-8') as gs:
@@ -72,7 +72,7 @@ async def testRepubblica():
     result = TokenLevelEval.eval(parsed_text_cleaned,gold_text)
     print(f"{ret["title"]} : {result:2f}")
 
-    ret = await parser.parse("https://www.repubblica.it/spettacoli/tv-radio/2026/04/10/news/cesaroni_il_ritorno_serie_tv_claudio_amendola_cast-425274728/")
+    ret = await parser.parse_by_url("https://www.repubblica.it/spettacoli/tv-radio/2026/04/10/news/cesaroni_il_ritorno_serie_tv_claudio_amendola_cast-425274728/")
     parsed_text_cleaned = MarkdownCleaner.clean(ret["parsed_text"])
     
     with open('GS/Repubblica.json', 'r', encoding='utf-8') as gs:
@@ -81,7 +81,7 @@ async def testRepubblica():
     result = TokenLevelEval.eval(parsed_text_cleaned,gold_text)
     print(f"{ret["title"]} : {result:2f}")
 
-    ret = await parser.parse("https://www.repubblica.it/salute/2024/12/05/news/l_influenza_che_non_passa_casi_in_aumento_cosa_succede_al_nostro_sistema_immunitario-423824877/")
+    ret = await parser.parse_by_url("https://www.repubblica.it/salute/2024/12/05/news/l_influenza_che_non_passa_casi_in_aumento_cosa_succede_al_nostro_sistema_immunitario-423824877/")
     parsed_text_cleaned = MarkdownCleaner.clean(ret["parsed_text"])
     
     with open('GS/Repubblica.json', 'r', encoding='utf-8') as gs:
@@ -90,7 +90,7 @@ async def testRepubblica():
     result = TokenLevelEval.eval(parsed_text_cleaned,gold_text)
     print(f"{ret["title"]} : {result:2f}")
 
-    ret = await parser.parse("https://www.repubblica.it/economia/2024/04/13/news/la_benzina_non_si_ferma_piu_punte_di_28_euro_al_litro_il_governo_prezzo_medio_sotto_2_euro-422520446/")
+    ret = await parser.parse_by_url("https://www.repubblica.it/economia/2024/04/13/news/la_benzina_non_si_ferma_piu_punte_di_28_euro_al_litro_il_governo_prezzo_medio_sotto_2_euro-422520446/")
     parsed_text_cleaned = MarkdownCleaner.clean(ret["parsed_text"])
     
     with open('GS/Repubblica.json', 'r', encoding='utf-8') as gs:
@@ -99,7 +99,7 @@ async def testRepubblica():
     result = TokenLevelEval.eval(parsed_text_cleaned,gold_text)
     print(f"{ret["title"]} : {result:2f}")
 
-    ret = await parser.parse("https://www.repubblica.it/il-gusto/2024/09/14/news/suppli_migliori_a_roma_dove_mangiarli-423499807/")
+    ret = await parser.parse_by_url("https://www.repubblica.it/il-gusto/2024/09/14/news/suppli_migliori_a_roma_dove_mangiarli-423499807/")
     parsed_text_cleaned = MarkdownCleaner.clean(ret["parsed_text"])
     
     with open('GS/Repubblica.json', 'r', encoding='utf-8') as gs:
@@ -111,7 +111,7 @@ async def testRepubblica():
 
 async def testPeople():
     parser = ParserPeople()
-    ret = await parser.parse("https://people.com/oscars-2024-everything-to-know-8548056")
+    ret = await parser.parse_by_url("https://people.com/oscars-2024-everything-to-know-8548056")
     parsed_text_cleaned = MarkdownCleaner.clean(ret["parsed_text"])
     
     with open('GS/People.json', 'r', encoding='utf-8') as gs:
@@ -120,7 +120,7 @@ async def testPeople():
     result = TokenLevelEval.eval(parsed_text_cleaned,gold_text)
     print(f"{ret["title"]} : {result:2f}")
 
-    ret = await parser.parse("https://people.com/hilarie-burton-searches-for-justice-in-season-3-of-true-crime-story-it-couldnt-happen-here-exclusive-11909272")
+    ret = await parser.parse_by_url("https://people.com/hilarie-burton-searches-for-justice-in-season-3-of-true-crime-story-it-couldnt-happen-here-exclusive-11909272")
     parsed_text_cleaned = MarkdownCleaner.clean(ret["parsed_text"])
     
     with open('GS/People.json', 'r', encoding='utf-8') as gs:
@@ -129,7 +129,7 @@ async def testPeople():
     result = TokenLevelEval.eval(parsed_text_cleaned,gold_text)
     print(f"{ret["title"]} : {result:2f}")
 
-    ret = await parser.parse("https://people.com/2025-will-look-more-normal-for-royal-family-than-2024-8735207")
+    ret = await parser.parse_by_url("https://people.com/2025-will-look-more-normal-for-royal-family-than-2024-8735207")
     parsed_text_cleaned = MarkdownCleaner.clean(ret["parsed_text"])
     
     with open('GS/People.json', 'r', encoding='utf-8') as gs:
@@ -138,7 +138,7 @@ async def testPeople():
     result = TokenLevelEval.eval(parsed_text_cleaned,gold_text)
     print(f"{ret["title"]} : {result:2f}")
 
-    ret = await parser.parse("https://people.com/what-happened-to-alissa-turney-8725736")
+    ret = await parser.parse_by_url("https://people.com/what-happened-to-alissa-turney-8725736")
     parsed_text_cleaned = MarkdownCleaner.clean(ret["parsed_text"])
     
     with open('GS/People.json', 'r', encoding='utf-8') as gs:
@@ -147,7 +147,7 @@ async def testPeople():
     result = TokenLevelEval.eval(parsed_text_cleaned,gold_text)
     print(f"{ret["title"]} : {result:2f}")
 
-    ret = await parser.parse("https://people.com/blake-lively-reveals-her-hidden-talent-is-interior-design-8421811")
+    ret = await parser.parse_by_url("https://people.com/blake-lively-reveals-her-hidden-talent-is-interior-design-8421811")
     parsed_text_cleaned = MarkdownCleaner.clean(ret["parsed_text"])
     
     with open('GS/People.json', 'r', encoding='utf-8') as gs:
