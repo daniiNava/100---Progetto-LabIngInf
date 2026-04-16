e3c6ae4c1fd71b11bbdd4705ffb458bad079e501
vrt88 <140164658+vrt88@users.noreply.github.com>
2026-04-16 19:07:09 +0200

    Correzione calculate_metrics

--- backend/src/services/evaluation_service.py
+++ backend/src/services/evaluation_service.py
@@ -13,12 +13,23 @@ def calculate_metrics(parsed_text : str, gold_text : str) -> Dict[str, float]:
     tokens_estratti = _tokenize(MarkdownCleaner.clean(parsed_text))
     tokens_gs = _tokenize(gold_text)
     n_correct_words = 0
-    for i in range(min(len(tokens_estratti),len(tokens_gs))):
-        if tokens_estratti[i] == tokens_gs[i]: n_correct_words += 1
-        else: print(f"{tokens_estratti[i]} {tokens_gs[i]} {i}")
-    precision = n_correct_words/len(tokens_estratti)
-    recall = n_correct_words/len(tokens_gs)
-    f1 = 2 * precision * recall / (precision + recall)
+    precision = 0
+    recall = 0
+    f1 = 0
+    len_token_estratti = len(tokens_estratti)
+    len_token_gs = len(tokens_gs)
+    if len_token_estratti != 0 and len_token_gs != 0:
+        for i in range(len_token_estratti):
+            token = tokens_estratti[0]
+            if token in tokens_gs:
+                tokens_gs.remove(token) 
+                n_correct_words += 1
+            else: 
+                print(f"{tokens_estratti[0]} {i}")
+            tokens_estratti.remove(token)
+        precision = n_correct_words/len_token_estratti
+        recall = n_correct_words/len_token_gs
+        f1 = 2 * precision * recall / (precision + recall)
     return{
         "precision": round(precision, 4),
         "recall": round(recall, 4),
