40435f05ec4d4845f14c95999314bc5c5a89f91b
Ray1745 <267098866+Ray1745@users.noreply.github.com>
2026-04-24 17:28:30 +0200

    modifica dei vari file json, mancano 5 url del dominio repubblica piu eliminazione file markdowncleaner

--- backend/src/markdownCleaner.py
+++
@@ -1,5 +0,0 @@
-class MarkdownCleaner():
-    @staticmethod
-    def clean(markdown_text : str) -> str:
-        cleaned_text = markdown_text.replace("#","").replace("_", "").replace("*", "")
-        return cleaned_text
